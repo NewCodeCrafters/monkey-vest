604c5773feb1b25c82602e9db045c793737b3160
divine <ogochukwudivine222@gmail.com>
2024-11-07 14:23:27 +0100

    corrected

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 
+
 from .models import Accounts
+
+
 
 
 class AccountsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Accounts
-        fields = "__all__"
+        fields = [
+            user = 'The user',
+            currency = 'NGN',
+            account_type = 'MAIN',
+            account_status = 'ACTIVE',
+        ]  
+            
+        
         
