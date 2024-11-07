5b2f6be0ecb603e4c9922d2d0e5398bc2fce7a62
onikageyoshi <davidonyekachi29@email.com>
2024-11-08 02:09:32 +0300

    custom admin added

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Accounts
 
-admin.site.register(Accounts)
+@admin.register(Accounts)
+class AccountsAdmin(admin.ModelAdmin):
+    list_display = ["account_id", "account_number","currency", "account_status"]
+    list_filter = ["account_id", "account_number","currency", "account_status"]
+    search_fields = list_display
