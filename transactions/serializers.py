5da26d8794a3c414d3871c5a2f7d5281a47e2f0d
divine <ogochukwudivine222@gmail.com>
2024-11-14 11:45:48 +0100

    views for withdrawal serializer

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -14,3 +14,12 @@ class DepositSerializer(serializers.ModelSerializer):
     class Meta:
         model = Deposit
         fields = "__all__"
+
+class WithdrawalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transfer
+        fields = '__all__'
+
+class CreateWithdrawalSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('account_number','amount')                 
--- transactions/urls.py
+++ transactions/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('deposit-history/', views.UserDepositView.as_view()),
     path('deposit/', views.CreateDepositView.as_view()),
+    path('withdrawal/', views.CreateWithdrawalView.as_view()),
+    path('withdrawal-history/', views.UserWithdrawalView.as_view())
 
 ]
