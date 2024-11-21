5967a85aeaa823099ad60d26d4e13aebd6cd604b
officialalkenes <belloabdulhakeemolamide@gmail.com>
2024-11-21 16:10:29 +0100

    chore: updated

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -29,3 +31,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('transactions/', include('transactions.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
