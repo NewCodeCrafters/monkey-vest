e3535f71a48feb809676c843f0f40156f0c29e35
officialalkenes <belloabdulhakeemolamide@gmail.com>
2024-11-21 15:10:14 +0100

    chore: update

--- core/settings.py
+++ core/settings.py
@@ -153,6 +153,10 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Additional directories where Django will search for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
