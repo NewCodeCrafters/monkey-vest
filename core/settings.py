7d686ebdd42f76268a228ba7128e5f7e6e456e80
officialalkenes <belloabdulhakeemolamide@gmail.com>
2024-11-23 14:15:10 +0100

    update static

--- core/settings.py
+++ core/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Additional directories where Django will search for static files
 STATICFILES_DIRS = [
